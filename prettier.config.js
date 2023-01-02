dbbb8f8d910921d091da14d7d36836492e4e7654
ackmanx <ackmanx@users.noreply.github.com>
2023-01-01 19:25:40 -0600

    Initial commit with electron, electron forge, empty app and prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  jsxSingleQuote: true,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  importOrder: ['^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
---
+++ renderer.js
@@ -0,0 +1,1 @@
+console.log(777, 'renderer')
