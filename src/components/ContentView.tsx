cc5261e7f068b8b7a2dd049000aeac396da4ac60
ackmanx <ackmanx@users.noreply.github.com>
2023-01-04 23:44:41 -0600

    Trying out gallery and lightbox library so i don't need to make them

--- src/components/ContentView.tsx
+++ src/components/ContentView.tsx
@@ -1,15 +1,47 @@
-import { Image } from './Image'
+import { useState } from 'react'
+import PhotoAlbum from 'react-photo-album'
+import { Lightbox } from 'yet-another-react-lightbox'
+import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen'
+import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails'
+import 'yet-another-react-lightbox/plugins/thumbnails.css'
+import Zoom from 'yet-another-react-lightbox/plugins/zoom'
+import 'yet-another-react-lightbox/styles.css'
 
 interface Props {
   images: string[]
 }
 
 export const ContentView = ({ images }: Props) => {
+  const [index, setIndex] = useState(-1)
+
+  const slides = images.map((image) => ({
+    src: `bs://${image}`,
+    // width: 3000,
+    // height: 3000,
+    // srcSet: [
+    //   {
+    //     src: `bs://${image}`,
+    //     width: 3000,
+    //     height: 3000,
+    //   },
+    // ],
+  }))
+
   return (
     <div style={{ flexGrow: 1 }}>
-      {images.map((image) => (
-        <Image key={image} path={image} />
-      ))}
+      <PhotoAlbum
+        layout='rows'
+        photos={images.map((image) => ({ src: `bs://${image}`, width: 200, height: 200 }))}
+        onClick={({ index }) => setIndex(index)}
+      />
+
+      <Lightbox
+        slides={slides}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        plugins={[Fullscreen, Thumbnails, Zoom]}
+      />
     </div>
   )
 }
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,55 +1,44 @@
 import { EyeOutlined } from '@ant-design/icons'
-import { useState } from 'react'
 
 interface Props {
   path: string
 }
 
 export const Image = ({ path }: Props) => {
-  const [showPreview, setShowPreview] = useState(false)
-
-  const handlePreview = (shouldShow: boolean) => {
-    setShowPreview(shouldShow)
-  }
-
   return (
-    <div style={{ display: 'inline-block', position: 'relative' }}>
-      <img
-        style={{
-          objectFit: 'cover',
-          width: '250px',
-          height: '250px',
-          margin: '8px',
-          cursor: 'pointer',
-        }}
-        src={`bs://${path}`}
-      />
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          width: '250px',
-          height: '50px',
-          margin: '8px',
-          backgroundColor: 'rgba(0, 0, 0, .65)',
-        }}
-      >
+    <div style={{ display: 'inline-block' }}>
+      <div style={{ position: 'relative' }}>
+        <img
+          style={{
+            objectFit: 'cover',
+            width: '250px',
+            height: '250px',
+            margin: '8px',
+          }}
+          src={`bs://${path}`}
+        />
         <div
           style={{
-            display: 'flex',
-            justifyContent: 'right',
-            paddingTop: '8px',
-            paddingRight: '8px',
+            position: 'absolute',
+            top: 0,
+            width: '250px',
+            height: '50px',
+            margin: '8px',
+            backgroundColor: 'rgba(0, 0, 0, .65)',
           }}
         >
-          <EyeOutlined
-            style={{ fontSize: '28px', color: '#bfc1c5' }}
-            onClick={() => handlePreview(!showPreview)}
-          />
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'right',
+              paddingTop: '8px',
+              paddingRight: '8px',
+            }}
+          >
+            <EyeOutlined style={{ fontSize: '28px', color: '#bfc1c5', cursor: 'pointer' }} />
+          </div>
         </div>
       </div>
-
-      {showPreview && <img src={`bs://${path}`} />}
     </div>
   )
 }
