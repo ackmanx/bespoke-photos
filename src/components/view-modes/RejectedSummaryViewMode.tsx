b79949801a7c4b85d3599eabb47f94dbd123231e
ackmanx <ackmanx@users.noreply.github.com>
2023-01-06 23:57:26 -0600

    Name and Path

--- src/components/view-modes/RejectedSummaryViewMode.tsx
+++ src/components/view-modes/RejectedSummaryViewMode.tsx
@@ -1,9 +1,6 @@
-import { Typography } from 'antd'
 import React from 'react'
 
 import { Color } from '../theme'
-
-const { Title } = Typography
 
 interface Props {
   rejectedPhotos: string[]
@@ -13,34 +10,36 @@ export const RejectedSummaryViewMode = ({ rejectedPhotos }: Props) => {
   return (
     <div style={{ padding: '8px 16px 0 16px' }}>
       <div style={{ overflow: 'scroll', height: 'calc(100vh - 8px)' }}>
-        <Title level={3} style={{ color: Color.fontColor }}>
-          Summary of Rejected
-        </Title>
-        {rejectedPhotos.map((src) => (
-          <div
-            style={{
-              display: 'flex',
-              width: '500px',
-              backgroundColor: Color.backgroundLight,
-              borderRadius: '10px',
-            }}
-          >
-            <img
-              key={src}
-              src={`bs://${src}`}
+        <h2 style={{ fontSize: '18px' }}>Summary of Rejected</h2>
+        {rejectedPhotos.map((src) => {
+          const b = src.split('/')
+
+          return (
+            <div
               style={{
-                objectFit: 'cover',
-                width: '250px',
-                height: '250px',
+                display: 'flex',
+                width: '500px',
+                backgroundColor: Color.backgroundLight,
                 borderRadius: '10px',
               }}
-            />
-            <div style={{ padding: '16px' }}>
-              <Title level={5}>Location:</Title>
-              {src}
+            >
+              <img
+                key={src}
+                src={`bs://${src}`}
+                style={{
+                  objectFit: 'cover',
+                  width: '250px',
+                  height: '250px',
+                  borderRadius: '10px',
+                }}
+              />
+              <div style={{ padding: '16px' }}>
+                <p>{b[b.length - 1]}</p>
+                <p>{b.slice(0, -1).join('/')}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     </div>
   )
