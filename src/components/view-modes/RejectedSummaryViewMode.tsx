685c89e51cedd93fba1ad93562156b87abeaa229
ackmanx <ackmanx@users.noreply.github.com>
2023-01-07 12:52:22 -0600

    Styling

--- src/components/view-modes/RejectedSummaryViewMode.tsx
+++ src/components/view-modes/RejectedSummaryViewMode.tsx
@@ -9,35 +9,42 @@ interface Props {
 
 export const RejectedSummaryViewMode = ({ rejectedPhotos }: Props) => {
   return (
-    <div style={{}}>
+    <div>
       <RejectedSummaryActionBar rejectedPhotos={rejectedPhotos} />
 
-      <div style={{ overflow: 'scroll', height: '94vh' }}>
+      <div style={{ overflow: 'scroll', height: '94vh', textAlign: 'center', paddingTop: '8px' }}>
         {rejectedPhotos.map((src) => {
           const splitSrc = src.split('/')
 
           return (
             <div
               style={{
-                display: 'flex',
+                display: 'inline-block',
                 width: '500px',
                 backgroundColor: Color.backgroundLight,
                 borderRadius: '10px',
+                textAlign: 'left',
               }}
             >
-              <img
+              <div
                 key={src}
-                src={`bs://${src}`}
                 style={{
-                  objectFit: 'cover',
-                  width: '250px',
-                  height: '250px',
-                  borderRadius: '10px',
+                  display: 'flex',
                 }}
-              />
-              <div style={{ padding: '16px' }}>
-                <p>{splitSrc[splitSrc.length - 1]}</p>
-                <p>{splitSrc.slice(0, -1).join('/')}</p>
+              >
+                <img
+                  src={`bs://${src}`}
+                  style={{
+                    objectFit: 'cover',
+                    width: '250px',
+                    height: '250px',
+                    borderRadius: '10px',
+                  }}
+                />
+                <div style={{ padding: '16px' }}>
+                  <p>{splitSrc[splitSrc.length - 1]}</p>
+                  <p>{splitSrc.slice(0, -1).join('/')}</p>
+                </div>
               </div>
             </div>
           )
