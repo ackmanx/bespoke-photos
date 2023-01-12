c53d40b583657ce5ba1fe65e1227d6d64588961b
ackmanx <ackmanx@users.noreply.github.com>
2023-01-11 23:48:44 -0600

    Added sharp libray to resize images for thumbnails

--- backend/load-directory.js
+++ backend/load-directory.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const $readdir = promisify(fs.readdir)
 const sizeOf = require('image-size')
+const sharp = require('sharp')
 
 async function handleLoadDirectory(event, directoryPath) {
   const files = await $readdir(directoryPath, { withFileTypes: true })
@@ -17,9 +18,21 @@ async function handleLoadDirectory(event, directoryPath) {
       const fullImagePath = path.resolve(directoryPath, image.name)
       const dimensions = sizeOf(fullImagePath)
 
+      sharp(fullImagePath)
+        .resize(250, 250)
+        .withMetadata()
+        .toFile(`${fullImagePath}--output.jpg`, function (err) {
+          if (err) {
+            console.error(777, 'error with:', image.name)
+            console.error(err)
+          }
+        })
+
       return {
         src: `bs://${fullImagePath}`,
         pureSrc: fullImagePath,
+        thumbSrc: `bs://${fullImagePath}`,
+        thumbPureSrc: fullImagePath,
         width: dimensions.width,
         height: dimensions.height,
       }
