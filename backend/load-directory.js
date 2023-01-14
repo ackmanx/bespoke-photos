53bdbb76c2a097ffe342811e065026274050b3f8
ackmanx <ackmanx@users.noreply.github.com>
2023-01-14 09:59:27 -0600

    Refactor

--- backend/load-directory.js
+++ backend/load-directory.js
@@ -9,42 +9,45 @@ const { STORAGE_PATH } = require('./utils')
 async function handleLoadDirectory(event, directoryPath) {
   const files = await $readdir(directoryPath, { withFileTypes: true })
 
-  return files
-    .filter((file) => {
-      const [extension] = file.name.split('.').slice(-1)
+  const fsEntryImages = files.filter((file) => {
+    const [extension] = file.name.split('.').slice(-1)
 
-      return file.isFile() && ['jpg', 'jpeg'].includes(extension)
-    })
-    .map((image) => {
-      const fullImagePath = path.resolve(directoryPath, image.name)
-      const thumbnailPath = `${STORAGE_PATH}/${fullImagePath.replaceAll('/', '|')}`
+    return file.isFile() && ['jpg', 'jpeg'].includes(extension)
+  })
 
-      const dimensions = sizeOf(fullImagePath)
+  //todo majerus: make this a loop that allows await
+  const images = fsEntryImages.map((image) => {
+    const fullImagePath = path.resolve(directoryPath, image.name)
+    const thumbnailPath = `${STORAGE_PATH}/${fullImagePath.replaceAll('/', '|')}`
 
-      if (!fs.existsSync(thumbnailPath)) {
-        // If a thumbnail doesn't exist in app storage, create one. The UI will point to this in the photo gallery
-        // This is async so it'll return before errors are processed but that's fine in this case
-        // Also, thumbnail path includes directory, so images that are moved will get new thumbnails
-        sharp(fullImagePath)
-          .resize(250, 250)
-          .withMetadata()
-          .toFile(`${thumbnailPath}`, function (err) {
-            if (err) {
-              console.error(777, 'error with:', image.name)
-              console.error(err)
-            }
-          })
-      }
+    const dimensions = sizeOf(fullImagePath)
 
-      return {
-        src: `bs://${fullImagePath}`,
-        pureSrc: fullImagePath,
-        thumbSrc: `bs://${thumbnailPath}`,
-        thumbPureSrc: `${thumbnailPath}`,
-        width: dimensions.width,
-        height: dimensions.height,
-      }
-    })
+    if (!fs.existsSync(thumbnailPath)) {
+      // If a thumbnail doesn't exist in app storage, create one. The UI will point to this in the photo gallery
+      // This is async so it'll return before errors are processed but that's fine in this case
+      // Also, thumbnail path includes directory, so images that are moved will get new thumbnails
+      sharp(fullImagePath)
+        .resize(250, 250)
+        .withMetadata()
+        .toFile(`${thumbnailPath}`, function (err) {
+          if (err) {
+            console.error(777, 'error with:', image.name)
+            console.error(err)
+          }
+        })
+    }
+
+    return {
+      src: `bs://${fullImagePath}`,
+      pureSrc: fullImagePath,
+      thumbSrc: `bs://${thumbnailPath}`,
+      thumbPureSrc: `${thumbnailPath}`,
+      width: dimensions.width,
+      height: dimensions.height,
+    }
+  })
+
+  return images
 }
 
 module.exports = {
